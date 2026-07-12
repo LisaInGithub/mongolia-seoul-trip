12da17b1755e26e46ea0d4cdc45361f328ff0054
Lisa Hsieh <88565760+LisaInGithub@users.noreply.github.com>
2026-07-12 15:41:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mongolia-seoul-trip-v1';
+const CACHE_NAME = 'mongolia-seoul-trip-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -26,7 +26,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    fetch(event.request)
+    fetch(event.request, { cache: 'no-store' })
       .then((res) => {
         const resClone = res.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
